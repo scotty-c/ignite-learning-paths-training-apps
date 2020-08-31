8f2cce38cb8d2f76ccff794ada1685341cf2646f
scott coulton <scott.coulton@microsoft.com>
2020-08-31 19:44:05 +1000

    take 3

--- apps40/deploy.sh
+++ apps40/deploy.sh
@@ -15,7 +15,7 @@ containerVersion=v2
 # Tailwind deployment
 tailwindInfrastructure=deployment.json
 tailwindCharts=TailwindTraders-Backend/Deploy/helm
-tailwindChartValuesScript=/helm-values/generate-config.ps1
+tailwindChartValuesScript=TailwindTraders-Backend/Deploy/powershell/generate-config.ps1
 tailwindChartValues=/values.yaml
 tailwindWebImages=TailwindTraders-Backend/Deploy/tt-images
 tailwindServiceAccount=TailwindTraders-Backend/Deploy/helm/ttsa.yaml
